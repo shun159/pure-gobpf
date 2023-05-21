a5d1abfa00c661ef5475d29e1f9869c011ae6e7c
Jayanth Varavani <1111446+jayanthvn@users.noreply.github.com>
2023-05-21 21:54:38 +0000

    Logs for debugging

--- pkg/ebpf_maps/map_loader.go
+++ pkg/ebpf_maps/map_loader.go
@@ -454,10 +454,13 @@ func (m *BPFMap) BulkRefreshMapEntries(newMapContents map[string]uintptr) error 
 	}
 
 	// 4. Delete stale Keys
+	log.Infof("Deleting stale entries and got %d entries from BPF map", len(retrievedMapKeyList))
 	for _, key := range retrievedMapKeyList {
+		log.Infof("Checking if key %s is deltable", key)
 		_, ok := newMapContents[key]
 		if !ok {
 			//This can be deleted since missing in new map data
+			log.Infof("This can be deleted, not needed anymore...")
 			deletableKeyByte := []byte(key)
 			deletableKeyBytePtr := uintptr(unsafe.Pointer(&deletableKeyByte[0]))
 			err = m.DeleteMapEntry(deletableKeyBytePtr)
